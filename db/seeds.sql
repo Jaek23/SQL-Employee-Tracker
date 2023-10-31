ec6a0632786959ed9cb13c73a32dbda238bfdca4
jae kim <jaehkim23@gmail.com>
2023-10-31 16:40:10 -0500

    added shcema/seeds file

---
+++ db/seeds.sql
@@ -0,0 +1,23 @@
+INSERT INTO department (department_name)
+VALUES
+('Sales'),
+('Finance'),
+('Legal'),
+('Engineering'),
+('Human Resources');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+('Sales Representative', 50000.00, 1),
+('Financial Analyst', 70000.00, 2),
+('Associate Attorney', 110000.00, 3),
+('Recruiter', 55000.00 ,4),
+('Mechanical Engineer', 125000.00, 5);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+('Jae', 'Kim', 1, 1),
+('James', 'Madison', 2, 2),
+('Grant', 'Williams', 3, 3),
+('Tony', 'Pollard', 4, 4),
+('Bryan','Gil', 5, 5);

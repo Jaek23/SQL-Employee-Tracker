4ad93f7b2d252401bc78fb3d09da16928cf40779
jae kim <jaehkim23@gmail.com>
2023-10-31 20:44:13 -0500

    working on getting table to show up on console

--- server.js
+++ server.js
@@ -11,3 +11,69 @@ const db = mysql.createConnection(
     },
     console.log(`Connected to the database.`)
   );
+
+  function generateTracker(){
+    inquirer
+        .prompt({
+            type:"list",
+            name:"tracker",
+            message:"What would you like to do?",
+            choices:[
+                "View all departments",
+                "View all roles",
+                "View all employess",
+                "add a department",
+                "add a role",
+                "add an employee",
+                "Update an employee role",
+                "Exit",
+            ],
+        })
+        .then((answer) =>{
+            switch(answer.action){
+                case "View all departments":
+                viewAllDepartments();
+                break;
+          
+                case "View all roles":
+                viewAllRoles();
+                break;   
+       
+                case "View all employees":
+                viewAllEmployees();
+                break;  
+          
+                case "add a department":
+                addADepartment();
+                break; 
+          
+                case "add a role":
+                addARole();
+                break;    
+     
+                case "add an employee":
+                addAnEmployee();
+                break; 
+          
+                case "Update an employee role":
+                updateAnEmployeeRole();
+                break; 
+         
+                case "Exit":
+                db.end();
+                console.log('Goodbye!');
+                break;
+            }
+
+        });
+    }
+generateTracker();
+
+function viewAllDepartments(){
+    const query = "SELECT * FROM department";
+    db.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        generateTracker();
+    });
+}
